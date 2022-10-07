1b04ac76b32de69121615f38963201c975967791
Harsh Sanghavi <harshpankajsanghai@gmail.com>
2022-10-07 11:29:47 +0530

    second commit

--- pattern.java
+++ pattern.java
@@ -10,6 +10,13 @@ public class pattern {
         pattern3(n3);
         int n4= sc.nextInt();
         pattern4(n4);
+        int size = sc.nextInt();
+        List<Character> l = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            char temp = sc.next();
+            l.add(temp);
+        }
+        sortChar(l,size);
     }
     static void pattern1(int n){
       for(int i=0;i<n;i++){
@@ -41,8 +48,43 @@ public class pattern {
     }
      static void pattern3(int n){
 
+     int mid  = n/2;
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==0 && i<mid) System.out.print("*");
+            else if(i==mid) System.out.print("*");
+            else if(j==mid) System.out.print("*");
+            else if(i==n-1 && j<mid) System.out.print("*");
+            else if(i==0 && j>mid) System.out.print("*");
+            else if(j==n-1 && i>mid) System.out.print("*");
+            else System.out.print(" ");
+        }
+        System.out.println();
+     }
     }
      static void pattern4(int n){
-
+    for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j==i)System.out.print("*");
+                else if(n-j-1==i) System.out.print("*");
+                else if(j==0)System.out.print("*");
+                else if(j==n-1)System.out.print("*");
+                else{System.out.print(" ");}
+            }System.out.println();
+        }
+    }
+    static void sortChar(ArrayList<Character> l , int n){
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(l.get(j)>l.get(i)){
+                    char temp = l.get(i);
+                    l.set(i,l.get(j));
+                    l.set(j,temp);
+                }
+            }
+        }
+         for(int i=0;i<n;i++){
+           System.out.print(l.get(i)+" ");
+        }
     }
 }
